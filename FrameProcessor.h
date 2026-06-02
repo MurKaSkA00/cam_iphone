fff15ab219062e3f5f24bb27099ee92cb4e053ea
MurKaSkA00 <LerotaMer87@outlook.com>
2026-06-02 08:34:14 -0400

    Create FrameProcessor.h

---
+++ FrameProcessor.h
@@ -0,0 +1,16 @@
+// FrameProcessor.h - MediaPlaybackUtils v1.4.2
+#import <Foundation/Foundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _MPUFrameProcessor : NSObject
+
++ (instancetype)sharedProcessor;
+- (CVPixelBufferRef _Nullable)processBuffer:(CVPixelBufferRef)src CF_RETURNS_RETAINED;
+- (CMSampleTimingInfo)jitteredTimingFromTiming:(CMSampleTimingInfo)t;
+
+@end
+
+NS_ASSUME_NONNULL_END
