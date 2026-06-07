6b40e280c0922d3af1aa3e7b32525cd99236f3ad
MurKaSkA00 <LerotaMer87@outlook.com>
2026-06-07 15:02:58 -0400

    Update MediaBufferAdapter.h

--- MediaBufferAdapter.h
+++ MediaBufferAdapter.h
@@ -11,14 +11,14 @@ typedef void (^_MPUErrorCallback)(NSError *error);
 
 @interface _MPUMediaBufferAdapter : NSObject <NSURLSessionDataDelegate>
 
-@property (nonatomic, strong, readonly) NSURL *streamURL;
-@property (nonatomic, assign, readonly) BOOL isConnecting;
-@property (nonatomic, assign, readonly) NSUInteger frameCount;
-@property (nonatomic, assign, readonly) CFAbsoluteTime lastFrameTime;
+@property (nonatomic, strong, readonly) NSURL          *streamURL;
+@property (nonatomic, assign, readonly) BOOL            isConnecting;
+@property (nonatomic, assign, readonly) NSUInteger      frameCount;
+@property (nonatomic, assign, readonly) CFAbsoluteTime  lastFrameTime;
 
-@property (nonatomic, copy, nullable) _MPUFrameCallback frameCallback;
-@property (nonatomic, copy, nullable) _MPUPixelBufferCallback pixelBufferCallback;
-@property (nonatomic, copy, nullable) _MPUErrorCallback errorCallback;
+@property (nonatomic, copy, nullable) _MPUFrameCallback        frameCallback;
+@property (nonatomic, copy, nullable) _MPUPixelBufferCallback  pixelBufferCallback;
+@property (nonatomic, copy, nullable) _MPUErrorCallback        errorCallback;
 
 - (instancetype)initWithURL:(NSURL *)url;
 - (void)startStreaming;
